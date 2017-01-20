a6b8f76f2d20a2c3674cc57c2c85f0449895255b
Melanie Gershman <melanie.gershman@gmail.com>
2017-01-20 17:06:35 -0500

    Print all images and colors

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'pg'
 gem 'rmagick'
 gem 'materialize-sass'
 
+gem 'pry-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
+require 'benchmark'
+
 class WelcomeController < ApplicationController
-  NUMBER_OF_IMAGES = 2
+  NUMBER_OF_IMAGES = 15
   def index
     collect_frequencies_of_images(NUMBER_OF_IMAGES)
     p @images
@@ -9,30 +11,33 @@ class WelcomeController < ApplicationController
 
   def collect_frequencies_of_images(number_of_images)
     @images = []
+
     number_of_images.times do |number|
       path = "app/assets/images/#{number}.jpg"
       image = load_image(path)
-      frequencies = collect_frequencies(image)
-      sorted_frequencies = sort_frequencies(frequencies)
-      @images << {top_colors: top_colors(sorted_frequencies)}
+
+      # Resize
+      # image = image.resize(0.3)
+
+      # binding.pry
+
+      # Pixelating
+      # less than 0.01 seconds
+      pixelated_image = pixelate(image)
+
+      # binding.pry
+      # takes 3 seconds
+      frequencies = collect_frequencies(pixelated_image)
+
+      # below: less than 0.01 seconds
+      @images << sort_frequencies(frequencies)
     end
+
     @images
   end
 
   def load_image(path)
      Magick::Image.read(path).first
-  end
-
-  def collect_frequencies(image)
-    frequencies = Hash.new(0)
-
-    pixels = pixelate(image)
-
-    pixels.map do |pixel|
-      frequencies[pixel.to_color] += 1
-    end
-
-    frequencies
   end
 
   def pixelate(image)
@@ -41,14 +46,35 @@ class WelcomeController < ApplicationController
     image.get_pixels(x_coordinate, y_coordinate, image.columns, image.rows)
   end
 
+  def collect_frequencies(pixelated_image)
+    frequencies = Hash.new(0)
+
+    pixelated_image.each do |pixel|
+      frequencies[pixel.to_color] += 1
+    end
+
+    frequencies
+  end
+
   def sort_frequencies(frequencies)
-    sorted = frequencies.sort_by {|_, frequency| frequency}
-    sorted.reverse
-  end
+    # Sort frequencies by frequency:
+    # # => { 109 => [red, blue, green], 5 => [yellow, black]}
+    sorted_frequencies = frequencies.group_by{ |color, pixel_count| pixel_count }
 
-  def top_colors(sorted_frequencies)
-    sorted_frequencies[0..4]
-  end
+    # based on sorted_frequencies, turn keys into an array, select maximum of 5 largest keys
+    top_five_keys = sorted_frequencies.keys.sort.pop(5).reverse
 
+    # create empty pixel_colors array to store 5 most commonly occuring pixel colors
+    pixel_colors = []
+
+    # until pixel_colors.length == 5, push values accessed from sorted_frequencies
+    top_five_keys.each do |key|
+      colors_array = sorted_frequencies[key]
+      colors_array.each { |color| pixel_colors.push(color[0]) }
+    end
+
+    # Keep only the top 5 colors (in case a key was associated with more than 1 color)
+    pixel_colors.shift(5)
+  end
 
 end
