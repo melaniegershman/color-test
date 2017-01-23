ba16483e482048ba21d5ca857bf0840af71d3be2
Melanie Gershman <melanie.gershman@gmail.com>
2017-01-22 19:40:32 -0500

    Print colors and images

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,45 +1,59 @@
 require 'benchmark'
 
 class WelcomeController < ApplicationController
-  NUMBER_OF_IMAGES = 15
+  IMAGES = []
+
   def index
-    collect_frequencies_of_images(NUMBER_OF_IMAGES)
-    # the below is for testing in terminal only
-    @hex_codes.each_with_index { |hex, index| p "The top 5 colors for image #{index} are: #{hex}"  }
+    @images = []
+    @images = create_image_array(@images)
+    @images = generate_image_colors(@images)
   end
 
   private
 
-  def collect_frequencies_of_images(number_of_images)
-    @hex_codes = []
-    number_of_images.times do |number|
-      number_of_colors = 256
-      path = "app/assets/images/#{number}.jpg"
-      image = load_image(path, number_of_colors)
-      total_depth = image.columns * image.rows
-      colors = get_hex(path, number_of_colors, total_depth)
-      colors = colors.sort_by {|color| color[:percent]}
-      colors.each {|hex_code| @hex_codes << hex_code[:hex]}
-      # binding.pry
+  def generate_image_colors(images)
+    number_of_colors = 256
+    images.each do |image|
+      image[:colors] = []
+      quantized_image = quantize_image(image[:path], number_of_colors)
+      total_pixel_count = quantized_image.columns * quantized_image.rows
+      colors = get_hex(image[:path], number_of_colors, total_pixel_count)
+      colors = colors.sort_by {|color| color[:percent]}.reverse
+
+      colors.each {|hex_code| image[:colors] << hex_code[:hex]}
+      image[:colors] =  image[:colors].shift(5)
     end
-    @hex_codes = @hex_codes.shift(5)
-    # binding.pry
+    images
   end
-# new
-  def load_image(path, number_of_colors)
+
+  def create_image_array(images)
+    image_directory = "app/assets/images"
+
+    Dir.foreach(image_directory) do |image|
+      image_object = {}
+      if image.include?(".jpg")
+        image_object[:filename] = image
+        image_object[:path] = image_directory + "/" + image
+        images << image_object
+      end
+    end
+    images
+
+  end
+
+  def quantize_image(path, number_of_colors)
     image = Magick::Image.read(path).first
-    image.quantize(number_of_colors, Magick::RGBColorspace)
+    quantized_image = image.quantize(number_of_colors, Magick::RGBColorspace)
   end
 
   def sort_color_histogram(path, number_of_colors)
-    quantized_image = load_image(path, number_of_colors)
+    quantized_image = quantize_image(path, number_of_colors)
     sorted_histogram = quantized_image.color_histogram.sort {|color, frequency| frequency[1] <=> color[1]}
   end
 
-  def get_hex(path, number_of_colors, total_depth)
+  def get_hex(path, number_of_colors, total_pixel_count)
     @hex_codes = []
     sorted_histogram = sort_color_histogram(path, number_of_colors)
-    # byebug
 
     sorted_histogram.count.times do |i|
       h = sorted_histogram[i]
@@ -57,51 +71,14 @@ class WelcomeController < ApplicationController
       b2 += b2 unless b2.length == 2
 
       hex = "#{r2}#{g2}#{b2}"
-      depth = h[1]
 
-      @hex_codes << {hex: hex, percent: ((depth.to_f/ total_depth.to_f)*100).round(2)}
+      pixel_count = h[1]
+      pixel_percent = ((pixel_count.to_f/ total_pixel_count.to_f)*100).round(2)
+
+      @hex_codes << {hex: hex, percent: pixel_percent}
     end
-    # an array of hashes containing hex code and percentage; may not need percentage
     @hex_codes
-    # binding.pry
+
   end
-
-# old
-  # def pixelate(image)
-  #   x_coordinate = 0
-  #   y_coordinate = 0
-  #   image.get_pixels(x_coordinate, y_coordinate, image.columns, image.rows)
-  # end
-
-  # def collect_frequencies(pixelated_image)
-  #   frequencies = Hash.new(0)
-  #
-  #   pixelated_image.each do |pixel|
-  #     frequencies[pixel.to_color] += 1
-  #   end
-  #
-  #   frequencies
-  # end
-
-  # def sort_frequencies(frequencies)
-  #   # Sort frequencies by frequency:
-  #   # # => { 109 => [red, blue, green], 5 => [yellow, black]}
-  #   sorted_frequencies = frequencies.group_by{ |color, pixel_count| pixel_count }
-  #
-  #   # based on sorted_frequencies, turn keys into an array, select maximum of 5 largest keys
-  #   top_five_keys = sorted_frequencies.keys.sort.pop(5).reverse
-  #
-  #   # create empty pixel_colors array to store 5 most commonly occuring pixel colors
-  #   pixel_colors = []
-  #
-  #   # until pixel_colors.length == 5, push values accessed from sorted_frequencies
-  #   top_five_keys.each do |key|
-  #     colors_array = sorted_frequencies[key]
-  #     colors_array.each { |color| pixel_colors.push(color[0]) }
-  #   end
-  #
-  #   # Keep only the top 5 colors (in case a key was associated with more than 1 color)
-  #   pixel_colors.shift(5)
-  # end
 
 end
